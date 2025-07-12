cf79d525c261857e6d2f96855267147fc0b0cd7f
Jannetta Steyn <6432530+jsteyn@users.noreply.github.com>
2025-07-12 11:23:59 +0100

    Remove empty line

--- src/main/java/uk/ac/ncl/dwa/model/Settings.java
+++ src/main/java/uk/ac/ncl/dwa/model/Settings.java
@@ -3,7 +3,6 @@ package uk.ac.ncl.dwa.model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.ac.ncl.dwa.database.DBHandler;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
