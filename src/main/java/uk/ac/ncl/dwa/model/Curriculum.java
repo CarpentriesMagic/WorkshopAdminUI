9335110a599eaa82669a3faacfa5983d6b604161
Jannetta Steyn <6432530+jsteyn@users.noreply.github.com>
2025-10-03 21:10:17 +0100

    Doing what I just, but again

--- src/main/java/uk/ac/ncl/dwa/model/Curriculum.java
+++ src/main/java/uk/ac/ncl/dwa/model/Curriculum.java
@@ -1,2 +1,23 @@
-package uk.ac.ncl.dwa.model;public class Curriculum {
+package uk.ac.ncl.dwa.model;
+
+public class Curriculum {
+
+    String curriculumID;
+    String description;
+
+    public String getCurriculumID() {
+        return curriculumID;
+    }
+
+    public void setCurriculumID(String curriculumID) {
+        this.curriculumID = curriculumID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- src/main/java/uk/ac/ncl/dwa/model/Flavour.java
+++ src/main/java/uk/ac/ncl/dwa/model/Flavour.java
@@ -1,2 +1,22 @@
-package uk.ac.ncl.dwa.model;public class Flavour {
+package uk.ac.ncl.dwa.model;
+
+public class Flavour {
+    String flavourID;
+    String description;
+
+    public String getFlavourID() {
+        return flavourID;
+    }
+
+    public void setFlavourID(String flavourID) {
+        this.flavourID = flavourID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
